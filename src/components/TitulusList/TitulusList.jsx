bf54719dbe734c931c322b1e585ae7240c1b1b5c
Serhii Rybak <rybaksw@gmail.com>
2023-12-02 00:00:05 +0200

    add static list add responsive btn

--- src/components/TitulusList/TitulusList.jsx
+++ src/components/TitulusList/TitulusList.jsx
@@ -8,18 +8,21 @@ import { HeroModal } from '@/components/ui-kit/HeroModal';
 
 export const TitulusList = ({ titulus, className = '' }) => {
   const [isClient, setIsClient] = useState(null);
+  const [amountOfList, setAmountOfList] = useState(4);
   const isDesktop = useMediaQuery({ minWidth: 1280 });
   const isTablet = useMediaQuery({ minWidth: 768 });
-
-  const amountOfList = isDesktop ? 9 : isTablet ? 12 : 4;
 
   useEffect(() => {
     setIsClient(true);
   }, []);
 
+  useEffect(() => {
+    setAmountOfList(isDesktop ? 9 : isTablet ? 12 : 4);
+  }, [isDesktop, isTablet]);
+
   return (
     <>
-      {isClient && (
+      {isClient ? (
         <ul
           className={classNames(
             'font-ui_montserrat text-ui_m_body1 md:text-ui_t_body1 xl:text-ui_d_body1 grid grid-col-1 gap-[6px]',
@@ -36,6 +39,43 @@ export const TitulusList = ({ titulus, className = '' }) => {
                 <HeroModal>
                   <ul className="grid gap-2 font-ui_montserrat text-ui_m_body1 md:text-ui_t_body1 xl:text-ui_d_body1 grid-col-1">
                     {titulus.slice(amountOfList).map(title => (
+                      <li
+                        key={title}
+                        className="relative pl-[31px] before:absolute before:left-0 before:content-sparkle text-left !whitespace-normal"
+                      >
+                        {title}
+                      </li>
+                    ))}
+                  </ul>
+                </HeroModal>
+              </li>
+            ) : (
+              <li
+                key={title}
+                className="relative pl-[31px] before:absolute before:left-0 before:content-sparkle text-left !whitespace-normal"
+              >
+                {title}
+              </li>
+            ),
+          )}
+        </ul>
+      ) : (
+        <ul
+          className={classNames(
+            'font-ui_montserrat text-ui_m_body1 md:text-ui_t_body1 xl:text-ui_d_body1 grid grid-col-1 gap-[6px]',
+            className,
+          )}
+        >
+          {titulus.slice(0, 4).map((title, idx, arr) =>
+            arr.length - 1 === idx ? (
+              <li
+                key={title}
+                className="relative pl-[31px] before:absolute before:left-0 before:content-sparkle text-left hitespace-normal"
+              >
+                <span className="mr-[10px] md:mr-3">{title}</span>
+                <HeroModal>
+                  <ul className="grid gap-2 font-ui_montserrat text-ui_m_body1 md:text-ui_t_body1 xl:text-ui_d_body1 grid-col-1">
+                    {titulus.slice(4).map(title => (
                       <li
                         key={title}
                         className="relative pl-[31px] before:absolute before:left-0 before:content-sparkle text-left !whitespace-normal"
