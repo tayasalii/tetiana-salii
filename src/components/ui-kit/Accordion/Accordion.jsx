928ee2b921003b9c5ad62d7a8ca466371a477ca2
Nadiia <holineinadia@gmail.com>
2023-12-09 15:36:36 +0200

    disable click at opened accordion

--- src/components/ui-kit/Accordion/Accordion.jsx
+++ src/components/ui-kit/Accordion/Accordion.jsx
@@ -17,6 +17,8 @@ export const Accordion = ({ items }) => {
   const itemRef = useRef(null);
 
   const onTitleClick = index => {
+    if (index === activeIndex) return;
+
     let prevItemIndx = null;
 
     if (index !== activeIndex) {
