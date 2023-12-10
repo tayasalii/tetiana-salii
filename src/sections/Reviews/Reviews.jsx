5aada04c62bf02420cf5ce8ca1adac7768cca249
marynaost <marynaum01@gmail.com>
2023-12-10 17:35:42 +0200

    fixed

--- src/sections/Reviews/Reviews.jsx
+++ src/sections/Reviews/Reviews.jsx
@@ -1,20 +1,22 @@
-'use client';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Keyboard, Navigation, Pagination } from 'swiper';
-import Image from 'next/image';
-import { useMediaQuery } from 'react-responsive';
+// import { Swiper, SwiperSlide } from 'swiper/react';
+// import { Keyboard, Navigation, Pagination } from 'swiper';
+// 'use client';
+
+// import Image from 'next/image';
+// import { useMediaQuery } from 'react-responsive';
 import { Container } from '@/components/Container';
+import { ReviewList } from '@/components/ReviewList';
 import { MainButton } from '@/components/ui-kit/MainButton';
 
 import slidesInfo from '@/data/slides.json';
 import btns from '@/data/buttons.json';
 
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+// import 'swiper/css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
 
 export const Reviews = () => {
-  const isDesktop = useMediaQuery({ minWidth: 1280 });
+  // const isDesktop = useMediaQuery({ minWidth: 1280 });
 
   return (
     <section className="section">
@@ -26,52 +28,7 @@ export const Reviews = () => {
 
       <div className="reviewBg bg-no-repeat bg-cover bg-center py-5  h-[397px] md:h-[414px] md:py-[30px] xl:bg-[length:1440px] xl:h-[667px]  xl:py-[50px]">
         <Container>
-          {isDesktop && (
-            <ul className="review-list">
-              {slidesInfo
-                .sort((a, b) => a.priority - b.priority)
-                .map((slide, index) => (
-                  <li key={index}>
-                    <Image
-                      src={slide.src}
-                      alt={slide.alt}
-                      width={295}
-                      height={322}
-                      className="w-auto h-auto"
-                    />
-                  </li>
-                ))}
-            </ul>
-          )}
-          {!isDesktop && (
-            <Swiper
-              className="sliderSwiper"
-              modules={[Keyboard, Navigation, Pagination]}
-              spaceBetween={20}
-              slidesPerView={1}
-              keyboard={{
-                enabled: true,
-              }}
-              pagination={{
-                clickable: true,
-              }}
-              navigation={true}
-              loop={true}
-              grabCursor={true}
-            >
-              {slidesInfo.map((slide, index) => (
-                <SwiperSlide key={index} style={{ display: 'flex' }}>
-                  <Image
-                    src={slide.src}
-                    alt={slide.alt}
-                    width={400}
-                    height={324}
-                    className="w-auto h-auto"
-                  />
-                </SwiperSlide>
-              ))}
-            </Swiper>
-          )}
+          <ReviewList slidesInfo={slidesInfo} />
         </Container>
       </div>
       <Container>
